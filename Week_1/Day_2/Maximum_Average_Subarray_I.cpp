06ba99f2029d047964a45eb4d8c38fee16afcaf2
sudheerkr-kr <sudheerkumar8076@gmail.com>
2026-06-02 10:40:18 +0530

    Implement Maximum Average Subarray I solution

---
+++ Week_1/Day_2/Maximum_Average_Subarray_I.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    double findMaxAverage(vector<int>& nums, int k) {
+
+        int sum = 0;
+
+        for(int i = 0; i < k; i++) {
+            sum += nums[i];
+        }
+
+        int maxSum = sum;
+
+        for(int i = k; i < nums.size(); i++) {
+
+            sum = sum - nums[i-k] + nums[i];
+
+            if(sum > maxSum) {
+                maxSum = sum;
+            }
+        }
+
+        return (double)maxSum / k;
+    }
+};
