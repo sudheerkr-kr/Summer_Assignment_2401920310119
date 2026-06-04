833363825edd2b94e1f32737501fdcbbdd9b0b6f
Sudheer kumar <sudheerkumar8076@gmail.com>
2026-06-04 23:11:38 +0530

    Create Reshape_The_Matrix.cpp

---
+++ Week_1/Day_4/Reshape_The_Matrix.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<vector<int>> matrixReshape(vector<vector<int>>& mat, int r, int c) {
+        
+        int rows = mat.size();
+        int cols = mat[0].size();
+
+        if (rows * cols != r * c) {
+            return mat;
+        }
+
+        vector<vector<int>> ans(r, vector<int>(c));
+
+        int idx = 0;
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+
+                ans[idx / c][idx % c] = mat[i][j];
+                idx++;
+            }
+        }
+
+        return ans;
+    }
+};
