684950e10fcc2e341cb3682b3cac603631a6e63b
Sudheer kumar <sudheerkumar8076@gmail.com>
2026-06-18 23:43:22 +0530

    Implement Evaluate Reverse Polish Notation solution

---
+++ Week_3/Part_B_Stack/Day_4/Evaluate_Reverse_Polish_Notation.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int evalRPN(vector<string>& tokens) {
+        int n = tokens.size();
+
+        stack<int> st;
+
+        for(string token: tokens){
+            if(token == "+" || token == "-" || token == "*" || token == "/"){
+                int num2 = st.top();
+                st.pop();
+
+                int num1 = st.top();
+                st.pop();
+
+                if(token == "+"){
+                    st.push(num1 + num2);
+                } 
+                else if(token == "-"){
+                    st.push(num1 - num2);
+                } 
+                else if(token == "*"){
+                    st.push(num1 * num2);
+                } 
+                else if(token == "/"){
+                    st.push(num1 / num2);
+                } 
+            }
+            else{
+                st.push(stoi(token));
+            }
+        }
+
+        return st.top();
+
+    }
+};
