d969ba562a8445dafe48b60860a08d0c0d7aec1f
Sudheer kumar <sudheerkumar8076@gmail.com>
2026-06-18 23:40:57 +0530

    Create Daily_Temperatures.cpp

---
+++ Week_3/Part_B_Stack/Day_4/Daily_Temperatures.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        int n = temperatures.size();
+
+        stack<int> st; //store indices
+        vector<int> ans(n , 0);
+
+        for(int i = 0; i < n; i++){
+            while(!st.empty() && temperatures[i] > temperatures[st.top()]){
+                int idx = st.top();
+                st.pop();
+
+                ans[idx] = i - idx;
+            }
+
+            st.push(i);
+        }
+
+        return ans;
+
+    }
+};
